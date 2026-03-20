4b954bf1722398e9890b753494546fd80fbef7cc
farres1 <sam.farrell@ons.gov.uk>
2026-03-20 14:48:06 +0000

    Add click function to button

---
+++ static/main.js
@@ -0,0 +1,10 @@
+const createSession = () => {
+  // TODO: Implement functionality
+  alert("Session created");
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("create-session-btn").addEventListener("click", () => {
+    createSession();
+  });
+});
