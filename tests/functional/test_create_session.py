297f3af7a56d4cf3d5228cd4535b0222fcb722e4
farres1 <sam.farrell@ons.gov.uk>
2026-04-08 13:43:50 +0100

    Update create session tests to use Python

---
+++ tests/functional/test_create_session.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import Page, expect
+
+def test_render_initial_page(page: Page):
+    page.goto("http://localhost:5100/")
+
+    expect(page).to_have_title("Collection Instrument Migration Service (CIMS)")
+    
+def test_confirmation_text_displayed_on_session_creation(page: Page):
+    page.goto("http://localhost:5100/")
+    
+    create_session_button = page.get_by_test_id("create-session-btn")
+    create_session_button.click()
+    
+    expect(page.get_by_text("Session created")).to_be_visible()
