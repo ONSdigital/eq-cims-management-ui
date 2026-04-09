9be00b09cd7f53e87a25829dd9de556aec50a79e
VirajP1002 <Viraj.Patel@ons.gov.uk>
2026-04-09 11:36:08 +0100

    Update test to check for class method call instead of repeating another test

--- tests/unit/database/test_firestore_logic.py
+++ tests/unit/database/test_firestore_logic.py
@@ -1,15 +1,14 @@
 """This module contains tests for the firestore_logic module to ensure business logic is working as expected."""
 
-import pytest
+from unittest import mock
 
 from eq_cims_management_ui.utils.database.firestore_logic import create_session
 
 
-@pytest.mark.usefixtures("mock_firestore_client")
-def test_create_session():
-    session = create_session()
+@mock.patch("eq_cims_management_ui.utils.database.firestore_logic.FirestoreHandler.create_new_session")
+def test_create_session(mock_create_new_session):
+    """Test that the create_session function calls the correct methods from the FirestoreHandler class."""
+    create_session()
 
-    assert session.get().to_dict() == {
-        "created_at": "2026-04-02 12:17:04.1775128624",
-        "status": "Not started",
-    }
+    assert mock_create_new_session.return_value is not None
+    assert mock_create_new_session.call_count == 1
