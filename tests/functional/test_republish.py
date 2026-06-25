6525b5cfcb9e72a24e24719226a90712c5dfadba
VirajP1002 <Viraj.Patel@ons.gov.uk>
2026-06-25 09:27:35 +0100

    Add functional tests for republish feature

---
+++ tests/functional/test_republish.py
@@ -0,0 +1,162 @@
+"""This module contains the functional tests for the Republish feature of the CIMS Management UI."""
+
+import json
+import re
+import time
+
+import pytest
+import requests
+from playwright.sync_api import Page, expect
+
+table_column_headers = ["Survey ID", "Form type", "CIR ID", "CIR version", "Validator version", "Status"]
+
+@pytest.fixture()
+def setup_cir():
+    """Fixture to set up a CIR for testing purposes by adding test collection instruments to CIR."""
+    schema_path = "tests/functional/test_ci.json"
+    with open(schema_path, encoding="utf-8") as f:
+        test_schema = json.load(f)
+
+    requests.post(
+        url="http://localhost:3030/collection-instruments",
+        params={"guid": "75f9d538-dda2-4852-ab6d-729391da2cdc", "validator_version": "0.0.1", "ci_version": "1"},
+        json=test_schema,
+        timeout=15,
+    )
+
+    requests.post(
+        url="http://localhost:3030/collection-instruments",
+        params={"guid": "35ef7238-d689-4285-adee-bd662a051f83", "validator_version": "0.0.1", "ci_version": "2"},
+        json=test_schema,
+        timeout=15,
+    )
+
+    requests.post(
+        url="http://localhost:3030/collection-instruments",
+        params={"guid": "cccac6bb-82de-4eca-9cee-6ca59b2751db", "validator_version": "0.0.1", "ci_version": "3"},
+        json=test_schema,
+        timeout=15,
+    )
+
+    yield
+
+    requests.delete(
+        url="http://localhost:3030/collection-instruments",
+        params={"survey_id": "123"},
+        timeout=15,
+    )
+
+
+def test_render_initial_page(page: Page):
+    """Verify that the initial page renders correctly."""
+    page.goto("http://localhost:5100/")
+
+    expect(page).to_have_title(re.compile(r"Collection Instrument Migration Service \(CIMS\)"))
+
+
+@pytest.mark.usefixtures("setup_cir")
+def test_display_content(page: Page):
+    """
+    Verify that clicking the create session button displays the expected content after making a request to CIR. Verify
+    that the republish button is disabled until the republish process is complete. Verify that the statuses of the CIs
+    are displayed correctly before and after the republish process.
+    """
+    page.goto("http://localhost:5100/")
+
+    create_session_button = page.get_by_test_id("create-session-btn")
+
+    create_session_button.click()
+
+    expect(page.get_by_role("heading", name=re.compile(r"Collection instruments"))).to_be_visible()
+
+    expect(page.get_by_role("table")).to_be_visible()
+    expect(page.get_by_text("75f9d538-dda2-4852-ab6d-729391da2cdc")).to_be_visible()
+    expect(page.get_by_text("35ef7238-d689-4285-adee-bd662a051f83")).to_be_visible()
+    expect(page.get_by_text("cccac6bb-82de-4eca-9cee-6ca59b2751db")).to_be_visible()
+
+    # Asserts that all column headers are visible in the table
+    for header in table_column_headers:
+        expect(page.get_by_role("columnheader", name=re.compile(header))).to_be_visible()
+
+    republish_button = page.get_by_test_id("republish-btn")
+
+    expect(page.get_by_text("Not started")).to_have_count(3)
+
+    republish_button.click()
+    expect(republish_button).to_be_disabled()
+
+    time.sleep(15)
+
+    expect(page.get_by_text("Success")).to_have_count(3)
+    expect(republish_button).to_be_disabled()
+
+
+@pytest.mark.usefixtures("setup_cir")
+def test_display_content_after_republish_complete(page: Page):
+    """
+    Verify that clicking the back button after a successful republish process returns the user to the create session
+    page.
+    """
+    page.goto("http://localhost:5100/")
+
+    create_session_button = page.get_by_test_id("create-session-btn")
+
+    create_session_button.click()
+
+    republish_button = page.get_by_test_id("republish-btn")
+    republish_button.click()
+
+    time.sleep(15)
+
+    page.go_back()
+
+    create_session_button.click()
+
+    expect(republish_button).to_be_enabled()
+    expect(page.get_by_text("Not started")).to_have_count(3)
+
+
+@pytest.mark.usefixtures("setup_cir")
+def test_display_content_republish_in_progress(page: Page):
+    """Verify that clicking the create session button displays the expected content after making a request to CIR."""
+    page.goto("http://localhost:5100/")
+
+    create_session_button = page.get_by_test_id("create-session-btn")
+
+    create_session_button.click()
+
+    republish_button = page.get_by_test_id("republish-btn")
+
+    republish_button.click()
+
+    time.sleep(1)
+
+    page.go_back()
+
+    create_session_button.click()
+
+    expect(republish_button).to_be_disabled()
+
+
+@pytest.mark.usefixtures("setup_cir")
+def test_display_content_republish_in_progress_after_closing_page(page: Page):
+    """Verify that clicking the create session button displays the expected content after making a request to CIR."""
+    page.goto("http://localhost:5100/")
+
+    create_session_button = page.get_by_test_id("create-session-btn")
+
+    create_session_button.click()
+
+    republish_button = page.get_by_test_id("republish-btn")
+
+    republish_button.click()
+
+    time.sleep(2)
+
+    page.close()
+
+    page.goto("http://localhost:5100/")
+
+    expect(republish_button).to_be_disabled()
+
+
