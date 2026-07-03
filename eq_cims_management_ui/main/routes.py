263b90cefd7691c09c8d79b87c42e46b3993ed67
VirajP1002 <Viraj.Patel@ons.gov.uk>
2026-07-03 10:25:25 +0100

    Add a new route for the results page which sends CI metadata to the results page, tweak emit_status to send validator version and error messages to the front end

--- eq_cims_management_ui/main/routes.py
+++ eq_cims_management_ui/main/routes.py
@@ -66,11 +66,18 @@ def handle_connect(auth: dict) -> None:
         join_room(session_id)
 
 
-def emit_status(guid: str, ci_status: str, session_id: str) -> None:
+def emit_status(guid: str, ci_status: str, session_id: str, validator_version: str, error_message: str) -> None:
     """Emit the status of a collection instrument and update the status in the database."""
     emit(
         "cell_update",
-        {"guid": guid, "status": ci_status, "index": 5, "suffix": STATUS_TO_SUFFIX[ci_status]},
+        {
+            "guid": guid,
+            "status": ci_status,
+            "index": 5,
+            "suffix": STATUS_TO_SUFFIX[ci_status],
+            "validator_version": validator_version,
+            "error_message": error_message,
+        },
         to=session_id,
     )
     update_ci_status(guid, ci_status)
@@ -90,10 +97,10 @@ def handle_republish() -> None:
     for ci in ci_metadata:
         guid = ci["cir_id"]
         if ci["status"] == "Success":
-            emit_status(guid, "Success", session_id)
+            emit_status(guid, "Success", session_id, ci["validator_version"], ci["error_message"])
             continue
 
-        emit_status(guid, "Started", session_id)
+        emit_status(guid, "Started", session_id, ci["validator_version"], ci["error_message"])
         try:
             response = requests.get(
                 f"http://{os.getenv("AUTHOR_REPUBLISH_API_URL")}/republishschema/{guid}/cirversion/{ci['cir_version']}",
@@ -105,7 +112,7 @@ def handle_republish() -> None:
             ci_status = "Failure"
 
         logger.error("%s republished CI: %s", "Successfully" if ci_status == "Success" else "Failed to", guid)
-        emit_status(guid, ci_status, session_id)
+        emit_status(guid, ci_status, session_id, ci["validator_version"], ci["error_message"])
 
     updated_ci_metadata = get_collection_instruments()
     if all(ci["status"] == "Success" for ci in updated_ci_metadata):
@@ -181,3 +188,29 @@ def get_view_session() -> ResponseReturnValue:
         return render_template("view-session.html", ci_metadata=ci_metadata, session_status=get_session_status())
     except AttributeError:
         return render_template("error.html", error_content=error_content_500), 500
+
+
+@main_blueprint.route("/result/<guid>", methods=["GET"])
+def get_result(guid):
+
+    ci_metadata = get_collection_instruments()
+
+    ci = next((ci for ci in ci_metadata if ci["cir_id"] == guid), None)
+
+    ci_status = ci["status"] if ci else "Not found"
+    survey_id = ci["survey_id"] if ci else "Not found"
+    form_type = ci["form_type"] if ci else "Not found"
+    version = ci["cir_version"] if ci else "Not found"
+    validator_version = ci["validator_version"] if ci else "Not found"
+    error_message = ci["error_message"] if ci else "Not found"
+
+    return render_template(
+        "result.html",
+        guid=guid,
+        status=ci_status,
+        survey_id=survey_id,
+        form_type=form_type,
+        version=str(version),
+        validator_version=validator_version,
+        error_message=error_message,
+    )
